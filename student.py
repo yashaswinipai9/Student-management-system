e17a133f18a744d6a3174e46896b05fca3de9587
yashaswinipai9 <yashaswini.pai@sdbi.in>
2026-07-18 02:54:21 +0530

    Introduced bug for demo of github actions

--- student.py
+++ student.py
@@ -13,7 +13,7 @@ def add_student(student_id, name, age):
         "age": age
     }
 
-    return "Student added successfully."
+    return "Student added."
 
 def remove_student(student_id):
     if student_id not in students:
