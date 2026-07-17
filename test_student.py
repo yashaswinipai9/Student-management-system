937c9239788a3cf1d4de97cfb73058b6317f95b3
yashaswinipai9 <yashaswini.pai@sdbi.in>
2026-07-17 20:16:44 +0530

    Added pytest test cases

---
+++ test_student.py
@@ -0,0 +1,82 @@
+from student import (
+    students,
+    add_student,
+    remove_student,
+    search_student,
+    update_student
+)
+
+def setup_function():
+    students.clear()
+
+def test_add_student():
+    result = add_student(101, "Rahul", 20)
+
+    assert result == "Student added successfully."
+    assert 101 in students
+
+def test_add_duplicate_student():
+    add_student(101, "Rahul", 20)
+
+    result = add_student(101, "Rahul", 20)
+
+    assert result == "Student already exists."
+
+def test_remove_student():
+    add_student(101, "Rahul", 20)
+
+    result = remove_student(101)
+
+    assert result == "Student removed successfully."
+    assert 101 not in students
+
+def test_remove_non_existing_student():
+    result = remove_student(999)
+
+    assert result == "Student not found."
+
+def test_search_existing_student():
+    add_student(101, "Rahul", 20)
+
+    result = search_student(101)
+
+    assert result == {
+        "name": "Rahul",
+        "age": 20
+    }
+
+def test_search_non_existing_student():
+    result = search_student(999)
+
+    assert result == "Student not found."
+
+def test_update_existing_student():
+    add_student(101, "Rahul", 20)
+
+    result = update_student(101, "Rahul Sharma", 21)
+
+    assert result == "Student updated successfully."
+
+    assert students[101]["name"] == "Rahul Sharma"
+    assert students[101]["age"] == 21
+
+def test_update_non_existing_student():
+    result = update_student(999, "ABC", 25)
+
+    assert result == "Student not found."
+
+def test_add_multiple_students():
+    add_student(101, "Rahul", 20)
+    add_student(102, "Priya", 21)
+
+    assert len(students) == 2
+
+def test_verify_updated_student():
+    add_student(101, "Rahul", 20)
+
+    update_student(101, "Rahul Sharma", 22)
+
+    result = search_student(101)
+
+    assert result["name"] == "Rahul Sharma"
+    assert result["age"] == 22
